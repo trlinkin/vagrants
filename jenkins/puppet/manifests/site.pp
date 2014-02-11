ef6ada7ad8c7ef4b46477b0eb13cde88d6d4794a
Thomas Linkin <trlinkin@gmail.com>
2014-02-10 21:43:24 -0500

    Establish ordering for jenkins modules

--- jenkins/puppet/manifests/site.pp
+++ jenkins/puppet/manifests/site.pp
@@ -5,9 +5,14 @@ node default{
     require => Class['java'],
   }
 
+  Jenkins::Plugin {
+    require => Class['jenkins'],
+  }
+
   jenkins::plugin { 'chucknorris': }
   jenkins::plugin { 'git': }
   jenkins::plugin { 'rbenv': }
+  jenkins::plugin { 'build-flow-plugin': }
 
   rbenv::install { "jenkins":
     group => 'jenkins',
