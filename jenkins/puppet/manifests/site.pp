5f148a69be853f781bd24734dad8af2ab55fc900
Thomas Linkin <trlinkin@gmail.com>
2014-09-30 16:29:14 -0400

    Refresh the jenkins environment

--- jenkins/puppet/manifests/site.pp
+++ jenkins/puppet/manifests/site.pp
@@ -1,21 +1,20 @@
 node default{
-  include java
-
   class {'jenkins':
-    require => Class['java'],
-  }
-
-  Jenkins::Plugin {
-    require => Class['jenkins'],
   }
 
   jenkins::plugin { 'chucknorris': }
-  jenkins::plugin { 'git': }
-  jenkins::plugin { 'rbenv': }
   jenkins::plugin { 'build-flow-plugin': }
-
-  rbenv::install { "jenkins":
-    group => 'jenkins',
-    home  => '/var/lib/jenkins',
+  jenkins::plugin { 'git': }
+  jenkins::plugin { 'ruby-runtime': }
+    require => Jenkins::Plugin['ruby-runtime']
   }
+
+  include git
+  include rvm
+  rvm::system_user {'jenkins':}
+
+  rvm_system_ruby { '1.9.3-p484':
+    ensure => 'present',
+  }
+
 }
